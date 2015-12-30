f51a3328a751905c76da7a2cf8b40736d880cff5
Rodrigo Villatoro <rodrigovillatoro@hotmail.com>
2015-12-30 18:28:28 +0100

    added new status column and alembic migrations

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Configuration
@@ -7,3 +9,7 @@ from config import Configuration
 app = Flask(__name__)
 app.config.from_object(Configuration)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
