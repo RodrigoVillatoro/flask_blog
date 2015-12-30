80f37b94c098a0a95af3eee5d9180f103bccc664
Rodrigo Villatoro <rodrigovillatoro@hotmail.com>
2015-12-30 12:53:58 +0100

    added db and script to create it

--- app/config.py
+++ app/config.py
@@ -4,4 +4,4 @@ import os
 class Configuration:
     APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite///{}/blog.db'.format(APPLICATION_DIR)
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/blog.db' % APPLICATION_DIR
