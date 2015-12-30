45c70147f5614791f2a3591e749a8bb385bf786c
Rodrigo Villatoro <rodrigovillatoro@hotmail.com>
2015-12-30 08:25:04 +0100

    created Entry model

--- app/config.py
+++ app/config.py
@@ -1,2 +1,7 @@
+import os
+
+
 class Configuration:
+    APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite///{}/blog.db'.format(APPLICATION_DIR)
