c2e1ebdad45353271e38968bc8e20fccf671f93d
Rodrigo Villatoro <rodrigovillatoro@hotmail.com>
2015-12-31 15:05:47 +0100

    added new tamplates and finished url scheme

---
+++ app/entries/blueprint.py
@@ -0,0 +1,36 @@
+from flask import render_template, Blueprint
+
+from helpers import object_list
+from models import Entry, Tag
+
+entries = Blueprint('entries', __name__, template_folder='templates')
+
+
+@entries.route('/')
+def index():
+    entries = Entry.query.order_by(Entry.created_timestamp.desc())
+    return object_list(template_name='entries/index.html',
+                       query=entries)
+
+
+@entries.route('/tags/')
+def tag_index():
+    tags = Tag.query.order_by(Tag.name)
+    return object_list(template_name='entries/tag_index.html',
+                       query=tags)
+
+
+@entries.route('/tags/<slug>')
+def tag_detail(slug):
+    tag = Tag.query.filter(Tag.slug == slug).first_or_404()
+    entries = tag.entries.order_by(Entry.created_timestamp.desc())
+    return object_list(template_name='entries/tag_detail.html',
+                       query=entries,
+                       tag=tag)
+
+
+@entries.route('/<slug>/')
+def detail(slug):
+    entry = Entry.query.filter(Entry.slug == slug).first_or_404()
+    return render_template(template_name_or_list='entries/detail.html',
+                           entry=entry)
