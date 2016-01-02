c5790e1a164821b838fef0cf202eb0f96a3189d4
Rodrigo Villatoro <rodrigovillatoro@hotmail.com>
2016-01-02 23:42:53 +0100

    images can now be uploaded

--- app/config.py
+++ app/config.py
@@ -2,8 +2,11 @@ import os
 
 from config_secret import *
 
+
 class Configuration:
     APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
     DEBUG = True
     SECRET_KEY = YOUR_SECRET_KEY
     SQLALCHEMY_DATABASE_URI = YOUR_DATABASE % APPLICATION_DIR
+    STATIC_DIR = os.path.join(APPLICATION_DIR, 'static')
+    IMAGES_DIR = os.path.join(STATIC_DIR, 'images')
