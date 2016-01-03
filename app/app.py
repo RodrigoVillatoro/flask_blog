e3f510eca11c4dda57b6090b5c67e4c83b11de03
Rodrigo Villatoro <rodrigovillatoro@hotmail.com>
2016-01-03 05:33:25 +0100

    added way tp store and verify user credentials

--- app/app.py
+++ app/app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, g
+from flask.ext.login import LoginManager, current_user
+
+from flask.ext.bcrypt import Bcrypt
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
@@ -13,3 +16,13 @@ migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+
+@app.before_request
+def _before_request():
+    g.user = current_user
+
+bcrypt = Bcrypt()
--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 import datetime
 import re
 
-from app import db
+from app import bcrypt, db, login_manager
 
 
 def slugify(title):
@@ -74,9 +74,54 @@ class User(db.Model):
     created_timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
 
     def __init__(self, *args, **kwargs):
-        super(User.self).__init__(*args, **kwargs)
+        super(User, self).__init__(*args, **kwargs)
         self.generate_slug()
+
+    def __repr__(self):
+        return '<User: {}>'.format(self.name)
 
     def generate_slug(self):
         if self.name:
             self.slug = slugify(self.name)
+
+    # Flask-Login interface...
+
+    def get_id(self):
+        return self.id
+
+    def is_authenticaded(self):
+        return True
+
+    def is_active(self):
+        return self.active
+
+    def is_anonymous(self):
+        return False
+
+    # Bcrypt-related
+
+    @staticmethod
+    def make_password(plaintext):
+        return bcrypt.generate_password_hash(plaintext)
+
+    def check_password(self, raw_password):
+        return bcrypt.check_password_hash(self.password_hash, raw_password)
+
+    @classmethod
+    def create(cls, email, password, **kwargs):
+        return User(
+            email=email,
+            password_hash=User.make_password(password),
+            **kwargs
+        )
+
+    @staticmethod
+    def authenticate(email, password):
+        user = User.query.filter(User.email == email).first()
+        if user and user.check_password(password):
+            return user
+        return False
+
+@login_manager.user_loader
+def _user_loader(user_id):
+    return User.query.get(int(user_id))
